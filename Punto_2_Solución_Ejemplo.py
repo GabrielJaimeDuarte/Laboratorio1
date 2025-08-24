eeaa859fd5bacb72e55a831d5c4f92d21389d213
GabrielJaimeDuarte <gabrieljaimeduarte@gmail.com>
2025-08-24 12:22:52 -0500

    Solución Ejemplo comprobacion punto 2

--- Punto_2_Solución_Ejemplo.py
+++ Punto_2_Solución_Ejemplo.py
@@ -1,7 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 24 12:09:24 2025
+estados = ["ENCENDIDA" , "APAGADA"]
 
-@author: gabod
-"""
+estado_actual = "APAGADA"
+estado_meta = "ENCENDIDA"
 
+def acciones_disponibles(estado):
+    return ["PRENDER" , "APAGAR"]
+
+def cambiar_estado(estado, accion):
+    if accion == "PRENDER":
+        return "ENCENDIDA"
+    elif accion == "APAGAR":
+        return "APAGADA"
+print(f"Estado inicial: {estado_actual}")
+
+accion = "PRENDER"
+estado_actual = cambiar_estado(estado_actual, accion)
+print(f"Después de {accion}: {estado_actual}")
+
+if estado_actual == estado_meta:
+    print("¡Meta alcanzada!")
+
+    
