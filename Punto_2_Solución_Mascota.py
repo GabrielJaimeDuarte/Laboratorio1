c85bdf504048a11991cdc53ba5e189d9e16abaf3
GabrielJaimeDuarte <gabrieljaimeduarte@gmail.com>
2025-08-24 12:35:05 -0500

    Solución Completa Mascota punto 2

--- Punto_2_Solución_Mascota.py
+++ Punto_2_Solución_Mascota.py
@@ -1,7 +1,23 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Aug 24 12:26:30 2025
+estados = ["CONTENTA" , "TRISTE"]
 
-@author: gabod
-"""
+estado_actual = "TRISTE"
+estado_meta = "CONTENTA"
 
+def acciones_disponibles(estado):
+    return ["RECIBIR UN PREMIO" , "NO RECIBE PREMIO"]
+
+def cambiar_estado(estado, accion):
+    if accion == "RECIBIR UN PREMIO":
+        return "CONTENTA"
+    elif accion == "TRISTE":
+        return "TRISTE"
+print(f"Estado inicial de la Mascota es: {estado_actual}")
+
+accion = "RECIBIR UN PREMIO"
+estado_actual = cambiar_estado(estado_actual, accion)
+print(f"Después de {accion}, ahora esta: {estado_actual}")
+
+if estado_actual == estado_meta:
+    print("¡Meta alcanzada!")
+
+    
